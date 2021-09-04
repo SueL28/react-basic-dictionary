818e0d80b9332d5da387704ddb5fd31b47907d70
SueL28 <69327405+SueL28@users.noreply.github.com>
2021-09-04 16:44:26 -0400

    added footer for open source code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import DictionaryApp from './DictionaryApp';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <DictionaryApp />
+        <Footer />
 
       </header>
     </div>
