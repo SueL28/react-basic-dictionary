d649b5a8ef19e1616498b4d3ab9c0b4b34ca792e
SueL28 <69327405+SueL28@users.noreply.github.com>
2021-09-04 17:16:12 -0400

    submitted search is logged and updating currently as what is being searched

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Search.css"
 
 export default function Search (){
+    let [keyword, setKeyword] = useState("");
+
+    function searchedWord(submit){
+        setKeyword(submit.target.value);
+    }
+
+    function searchSubmit(event){
+        event.preventDefault();
+        alert (`Searching for ${keyword}`);
+    }
 
     return(
         <div className="Search">
-            <form>
-                <input type="search" placeholder="Search to Define Word" className="search-bar"/>
+            <form onSubmit={searchSubmit}>
+                <input type="search" placeholder="Search to Define Word" className="search-bar" autoFocus={true} onChange={searchedWord}/>
                 <input type="submit" value="🔎" className="search-button"/>
             </form>
 
